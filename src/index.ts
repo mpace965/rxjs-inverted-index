d880ca574dfe4fee28812ff35ab7f256371404d7
Matt Pace <matthew.dp.96@gmail.com>
2018-11-03 19:52:48 -0700

    Initial line number reporting

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,28 @@
+import * as fs from 'fs';
+import * as readline from 'readline';
+import { fromEvent, Observable } from 'rxjs';
+import { map, takeUntil } from 'rxjs/operators';
 
-export const hello = 'hello';
-console.log(hello);
+interface ILineNumber {
+    lineNumber: number;
+    lineText: string;
+}
+
+const rl = readline.createInterface({
+    input: fs.createReadStream('sample.txt'),
+    crlfDelay: Infinity
+});
+
+const closeStream: Observable<void> = fromEvent<void>(rl, 'close');
+const lineStream: Observable<string> = fromEvent<string>(rl, 'line').pipe(takeUntil(closeStream));
+
+const lineNumberStream: Observable<ILineNumber> = lineStream.pipe(
+    map<string, ILineNumber>((value: string, index: number) => {
+        return {
+            lineNumber: index + 1,
+            lineText: value
+        }
+    })
+);
+
+lineNumberStream.subscribe(x => console.log(x));
